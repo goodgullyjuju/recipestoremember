d37c4937e9a44367a53882c10c70825e2dd45c06
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-02-15 16:47:41 -0500

    update to 'ContentView.js'

--- src/components/ContentView.js
+++ src/components/ContentView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './ContentView.css'; // Make sure to create this CSS file
+import ‘../ContentView.css'; // Make sure to create this CSS file
 
 function ContentView() {
   return (
