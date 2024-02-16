887af9e0b1209ccc4a42e1736beab6af9fa35015
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-02-15 22:25:42 -0500

    updated 'sampleRecipes.js'

--- src/sampleRecipes.js
+++ src/sampleRecipes.js
@@ -1,79 +1,78 @@
 const sampleRecipes = [
-    {
-        id: 1,
-        name: "Mom's Pizza",
-        description: "A homemade pizza with a secret family recipe.",
-        ingredients: [
-          "1 1/2 cups Sourdough Starter",
-          "3/4 cup Bread Flour",
-          "1/2 tsp Baking Powder",
-          "1/2 tsp Garlic Salt",
-          "1/2 tsp crushed Rosemary",
-          "1/4 tsp Ground Black Pepper",
-          "1x15 oz can Organic Tomato Sauce",
-          "1 Tbsp Organic Tomato Paste",
-          "5 Tbsp Extra Virgin Olive Oil (split 2/1/2)",
-          "1/2 tsp Dried Oregano",
-          "1 tsp Dried Basil",
-          "1 tsp Garlic Salt",
-          "Shredded Mozzarella Cheese",
-          "Pepperonis",
-          "Grated Italian Cheeses: Parmesan, Romano, Asiago (optional for extra flavor)"
-        ],
-        steps: [
-          "Preheat oven to 400° and place baking stone or cast iron pan in oven to preheat as well.",
-          "Make Pizza Crust:",
-          "Mix sourdough starter, bread flour, baking powder, and spices in a large bowl, preferably with a mixer.",
-          "Make Pizza Sauce:",
-          "Mix tomato sauce and tomato paste with remaining 2 Tbsp extra virgin olive oil, oregano, basil, and garlic salt. Heat on Medium high until warm. If you don’t have all of the ingredients, you can substitute your favorite store-bought pizza sauce to speed things up.",
-          "Remove baking stone or cast iron pan from oven and spread 2 Tbsp extra virgin olive oil on hot surface.",
-          "Wet fingers with water to keep dough from sticking to them and spread dough onto stone or pan all the way to the edges.",
-          "Put pizza dough into preheated oven and cook for 15 minutes until top is starting to get golden brown and puff up.",
-          "Take pizza dough out of oven and spread 1 Tbsp extra olive oil around the edges. Sprinkle with garlic salt for added flavor.",
-          "Spread pizza sauce on pizza dough.",
-          "Sprinkle mozzarella cheese.",
-          "Add pepperoni, more mozzarella cheese, and grated Italian cheeses if you have it.",
-          "Switch oven to Broil.",
-          "Broil pizza until the toppings get bubbly and the cheeses and pepperoni start browning.",
-          "Remove pizza from oven and pat with a paper towel to remove extra grease from pepperonis.",
-          "Cool, Cut, and Enjoy!"
-        ]
-        imageUrl: '/images/MomsPizza.jpeg',
-        videoUrl: "https://www.youtube.com/embed/t0H_0d7QBs8" // Converted to embed format
+  {
+    id: 1,
+    name: "Mom's Pizza",
+    description: "A homemade pizza with a secret family recipe.",
+    ingredients: [
+      "1 1/2 cups Sourdough Starter",
+      "3/4 cup Bread Flour",
+      "1/2 tsp Baking Powder",
+      "1/2 tsp Garlic Salt",
+      "1/2 tsp crushed Rosemary",
+      "1/4 tsp Ground Black Pepper",
+      "1x15 oz can Organic Tomato Sauce",
+      "1 Tbsp Organic Tomato Paste",
+      "5 Tbsp Extra Virgin Olive Oil (split 2/1/2)",
+      "1/2 tsp Dried Oregano",
+      "1 tsp Dried Basil",
+      "1 tsp Garlic Salt",
+      "Shredded Mozzarella Cheese",
+      "Pepperonis",
+      "Grated Italian Cheeses: Parmesan, Romano, Asiago (optional for extra flavor)"
+    ],
+    steps: [
+      "Preheat oven to 400° and place baking stone or cast iron pan in oven to preheat as well.",
+      "Make Pizza Crust:",
+      "Mix sourdough starter, bread flour, baking powder, and spices in a large bowl, preferably with a mixer.",
+      "Make Pizza Sauce:",
+      "Mix tomato sauce and tomato paste with remaining 2 Tbsp extra virgin olive oil, oregano, basil, and garlic salt. Heat on Medium high until warm. If you don’t have all of the ingredients, you can substitute your favorite store-bought pizza sauce to speed things up.",
+      "Remove baking stone or cast iron pan from oven and spread 2 Tbsp extra virgin olive oil on hot surface.",
+      "Wet fingers with water to keep dough from sticking to them and spread dough onto stone or pan all the way to the edges.",
+      "Put pizza dough into preheated oven and cook for 15 minutes until top is starting to get golden brown and puff up.",
+      "Take pizza dough out of oven and spread 1 Tbsp extra olive oil around the edges. Sprinkle with garlic salt for added flavor.",
+      "Spread pizza sauce on pizza dough.",
+      "Sprinkle mozzarella cheese.",
+      "Add pepperoni, more mozzarella cheese, and grated Italian cheeses if you have it.",
+      "Switch oven to Broil.",
+      "Broil pizza until the toppings get bubbly and the cheeses and pepperoni start browning.",
+      "Remove pizza from oven and pat with a paper towel to remove extra grease from pepperonis.",
+      "Cool, Cut, and Enjoy!"
+    ],
+    imageUrl: '/images/MomsPizza.jpeg', // Added missing comma here
+    videoUrl: "https://www.youtube.com/embed/t0H_0d7QBs8" // Converted to embed format
   },
-      {
-        id: 2,
-        name: "Mom's Chocolate Chip Cookies",
-        description: "Delicious chocolate cookies with pecans.",
-        ingredients: [
-          "1 1/2 sticks butter (room temperature)",
-          "1/2 cup organic cane sugar",
-          "1 cup organic light brown sugar (pressed)",
-          "1 1/2 tbsp vanilla",
-          "2 large eggs",
-          "2 cups unbleached all-purpose flour",
-          "1/2 tsp kosher salt",
-          "1 tsp baking soda",
-          "2 cups semi-sweet chocolate chips",
-          "2 cups chopped pecans",
-        ],
-        steps: [
-          "Preheat oven to 375 degrees.",
-          "Line cookie sheets with parchment paper.",
-          "Cream butter, cane sugar, and light brown sugar until smooth.",
-          "Add vanilla and eggs and mix until well incorporated.",
-          "In a separate bowl, whisk together flour, salt, and baking soda.",
-          "Add chocolate chips and pecans to flour mixture and combine well.",
-          "Mix dry ingredients in with wet ingredients just until combined.",
-          "Drop rounded tablespoons of batter on lined cookie sheets, making sure cookies are 2 inches apart.",
-          "Bake for 8-10 minutes or until golden brown.",
-          "Cool and serve.",
-          "Makes 30-36 cookies.",
-        ]
-        imageUrl: '/images/MomsChocolateChipCookies.jpeg',
-        videoUrl: "https://www.youtube.com/embed/4WieSJK5q7w" // Converted to embed format
+  {
+    id: 2,
+    name: "Mom's Chocolate Chip Cookies",
+    description: "Delicious chocolate cookies with pecans.",
+    ingredients: [
+      "1 1/2 sticks butter (room temperature)",
+      "1/2 cup organic cane sugar",
+      "1 cup organic light brown sugar (pressed)",
+      "1 1/2 tbsp vanilla",
+      "2 large eggs",
+      "2 cups unbleached all-purpose flour",
+      "1/2 tsp kosher salt",
+      "1 tsp baking soda",
+      "2 cups semi-sweet chocolate chips",
+      "2 cups chopped pecans",
+    ],
+    steps: [
+      "Preheat oven to 375 degrees.",
+      "Line cookie sheets with parchment paper.",
+      "Cream butter, cane sugar, and light brown sugar until smooth.",
+      "Add vanilla and eggs and mix until well incorporated.",
+      "In a separate bowl, whisk together flour, salt, and baking soda.",
+      "Add chocolate chips and pecans to flour mixture and combine well.",
+      "Mix dry ingredients in with wet ingredients just until combined.",
+      "Drop rounded tablespoons of batter on lined cookie sheets, making sure cookies are 2 inches apart.",
+      "Bake for 8-10 minutes or until golden brown.",
+      "Cool and serve.",
+      "Makes 30-36 cookies.",
+    ],
+    imageUrl: '/images/MomsChocolateChipCookies.jpeg', // Added missing comma here
+    videoUrl: "https://www.youtube.com/embed/4WieSJK5q7w" // Converted to embed format
   }
 ];
-    export default sampleRecipes;
- 
-     
+
+export default sampleRecipes;
