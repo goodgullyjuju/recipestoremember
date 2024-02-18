a5e5894631dd5b72bbe70a1a2f163b7f0c7d1c27
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-02-18 16:56:43 -0500

    update, remove, add images to 'images', update 'sampleRecipes'

--- src/sampleRecipes.js
+++ src/sampleRecipes.js
@@ -2,7 +2,7 @@ const sampleRecipes = [
   {
     id: 1,
     name: "MomsPizza",
-    description: "A homemade pizza with a secret family recipe.",
+    description: "A homemade pizza with a secret family recipe. (Makes 1 pizza)",
     ingredients: [
       "1 1/2 cups Sourdough Starter",
       "3/4 cup Bread Flour",
@@ -38,13 +38,12 @@ const sampleRecipes = [
       "Remove pizza from oven and pat with a paper towel to remove extra grease from pepperonis.",
       "Cool, Cut, and Enjoy!"
     ],
-    imageUrl: process.env.PUBLIC_URL + '/images/MomsPizza.jpeg', // Correct path
+    imageUrl: process.env.PUBLIC_URL + '/images/MomsPizza.png', // Correct path
     videoUrl: "https://www.youtube.com/embed/t0H_0d7QBs8" // Converted to embed format
   },
   {
-    id: 2,
     name: "MomsChocolateChipCookies",
-    description: "Delicious chocolate cookies with pecans.",
+    description: "Delicious chocolate cookies with pecans. (Makes about 3 dozen cookies depending on individual size)",
     ingredients: [
       "1 1/2 sticks butter (room temperature)",
       "1/2 cup organic cane sugar",
@@ -70,9 +69,202 @@ const sampleRecipes = [
       "Cool and serve.",
       "Makes 30-36 cookies.",
     ],
-    imageUrl: process.env.PUBLIC_URL + '/images/MomsChocolateChipCookies.jpeg', // Correct path
+    imageUrl: process.env.PUBLIC_URL + '/images/MomsChocolateChipCookies.png', // Correct path
     videoUrl: "https://www.youtube.com/embed/4WieSJK5q7w" // Converted to embed format
+  },
+{
+id: 3,
+    name: "Soft Pretzels Using Sourdough Discard",
+    description: "We make buttery soft pretzels using sourdough discard on Valentine’s Day 2023.  (Makes 8 soft pretzels)",
+    ingredients: [
+	"1 1/4 cups warm water",
+	"1 Tbsp brown sugar",
+	"1 1/4 tsp active dry yeast",
+	"1 cup Sourdough Discard",
+	"4 cups organic bread flour",
+	"2 tsp kosher salt",
+	"8 tbsp butter - melted",
+	"abut 10 cups of boiling water",
+	"2/3 cup baking soda",
+	"1 egg - just use the egg whites",
+	"2 Tbsp Coarse Kosher Salt or Pretzel Salt for topping",
+],
+steps: [
+	"Whisk together the warm water and brown sugar. Add yeast and let sit for 2-3 minutes until yeast blooms.",
+	"Whisk in sourdough discard to yeast bloom. Add melted butter when butter is cool to touch so you don’t kill yeast or sourdough - about 80 degrees.",
+	"Whisk together flour and salt. Mix in with water/yeast/sourdough/butter liquids in mixer with kneading hook",
+	"Once the dough comes together continue to knead in mixer with kneading hook for 5 minutes.",
+	"Remove dough from mixer and place in covered oiled bowl until dough doubles in size.",
+	"Once dough has doubled, cut into 8 equal parts. Roll out each pretzel into 1 inch strand and fold tips over to middle crossing each other. Press each end onto middle of pretzel strand and then press the two tip strands together in the middle. Set on Parchment lined cookie sheet, and let rest under warm damp cloth for 20 minutes.",
+	"Preheat oven to 425 degrees.",
+	"Bring water and 2/3 cup baking soda to boil. Drop Pretzels into boiling water 30 seconds each side. Place boiled pretzels back on parchment lined cookie sheet when done.",
+	"Brush pretzels with whisked egg whites, and then sprinkle lightly with coarse kosher salt or pretzel salt.",
+	"Bake at 425 degrees for 18 minutes.",
+	"Enjoy!",
+    ],
+
+ imageUrl: process.env.PUBLIC_URL + '/images/SoftPretzels.png',
+    videoUrl: "https://youtube.com/embed/ynRHkRIfuvw",
+},
+  {
+id: 4,
+    name: "Asiago Bagels",
+	description: "We are making bagels from sourdough discard and then topping with mixed grated Italian cheese or leaving them plain. (Makes 8 bagels)",
+	ingredients: [
+	"1 cup sourdough starter discard unfed",
+	"1 1/4 cup warm water",
+	"5 Tbsp sugar (divided)",
+	"1 teaspoon active dry yeast",
+	"3 3/4 cups organic bread flour",
+	"2 tsp kosher salt",
+	"10 cups boiling water",
+	"1 egg white",
+	"1/2 cup grated Italian Cheeses (asiago, parmesan, Romano, etc.",
+	"Olive oil (to coat rising bowl for dough)",
+],
+    steps: [
+	"In bowl of mixer, combine sourdough starter with warm water and 3 Tbsp sugar. Add active dry yeast and let sit for 2-3 minutes until yeast blooms. ",
+	"In separate large mixing bowl, whisk together bread flour and salt. Add to liquids in mixer one cup at a time until ball of dough forms.",
+	"Continue to knead dough in mixer with bread dough hook for 5 minutes.",
+	"Place dough in large oiled, covered bowl until doubled in size.",
+	"Remove dough from oiled bowl and cut into 8 equal pieces on cutting board. (Do not dust with flour or add any more flour.",
+	"Shape 8 pieces into bagels by hand, but do so gently. Do not squeeze air from dough. Place on parchment lined cookie sheet and cover with warm damp cloth for 20 minutes.",
+	"Preheat oven to 425 degrees.",
+	"Bring water to boil with 2 Tbsp sugar.",
+	"Boil each bagel 30 seconds each side and return to parchment lined sheet",
+	"Brush each bagel with egg white wash and dip in Asiago Italian cheese toppings.",
+	"Bake at 425 degrees for 18 minutes",
+	"Enjoy!",
+],
+ imageUrl: process.env.PUBLIC_URL + '/images/AsiagoBagels.png',
+    videoUrl: "https://youtube.com/embed/GprxujdCIPg", // Removed extra colon
+  },
+  {
+    id: 5,
+    name: "Crusty Sourdough Bread Loaf",
+	description:"Katie Lou makes a crusty sourdough loaf of bread in her Breville Bread Machine.",
+
+	ingredients: [
+	"1 1/4 cup sourdough starter discard unfed",
+	"3/4 cup and 1 Tbsp warm water",
+	"2 1/2 tsp kosher salt",
+	"3 cups and 2 Tbsp organic bread flour",
+	"1 1/2 Tbsp vital wheat gluten",
+	"3/4 tsp active dry yeast",
+	"Pam cooking spray",
+],
+steps: [
+	"Spray bread pan with Pam cooking spray",
+	"Add sourdough starter and warm water to pan",
+	"Whisk salt into flour and add to pan on top of liquids",
+	"Add vital wheat gluten and yeast on top of dry ingredients.",
+	"Place in Breville Bread Machine and select \"Crusty Loaf\", \"2 1/2 lb\",",
+	"Enjoy hot and fresh sourdough bread in about 3 hours and 40 minutes!",
+],
+    imageUrl: process.env.PUBLIC_URL + '/images/CrustyLoaf.png',
+    videoUrl: "https://youtube.com/embed/-7O_Up-Wc84", // Correct syntax
+  },
+  {
+    id: 6,
+    name: "Burger Pizza",
+	description:"We make a pizza from scratch topped with lean grass-fed ground beef seasoned with Italian herbs and spices. (Makes 2 pizzas)",
+
+	ingredients: [
+	"1 can Organic Tomato Sauce",
+	"1 Tbsp Organic Tomato Paste",
+	"10 Tbsp Extra Virgin Olive Oil - divided",
+ 	"1 tsp dried oregano - divided",
+	"2 tsp dried garlic powder - divided",
+     "2 tsp Dried Basil - divided",
+     "3 tsp Garlic Salt - divided",
+	"2 tsp dried parsley - divided",
+	"1/2 tsp dried ground black pepper - divided",
+	"3 cups sourdough starter discard unfed",
+	"1 1/2 cups organic bread flour",
+	"3 tsp kosher salt - divided",
+	"1.3 lbs organic grass-fed ground beef",
+	"4 cups shredded Mozzarella cheese",
+	"1/2 grated Italian cheeses (Asiago, Parmesan, Romano)",
+],
+ steps: [
+	"Preheat oven and pizza stones to 425 degrees.",
+	"Create Pizza Sauce by warming tomato sauce, tomato paste, 2 Tbsp Olive Oil, 1 tsp each of oregano, garlic, basil, and parsley. Also add 1 tsp garlic salt and 1/4 tsp black pepper. Cover and set aside.",
+	"Brown the ground beef and season with 1 tsp each of oregano, garlic salt, basil, and parsley. Also add 1/4 tsp black pepper. Drain any fat and set aside covered.",
+	"In a mixer, combine sourdough starter with bread flour and 1 tsp each of oregano, garlic, basil, and parsley. Also add 1/4 tsp black pepper and 1 tsp kosher salt. Mix well. Divide dough ball into 2 balls. These will be your pizza doughs.",
+	"Pull preheated pizza stone from oven and cover with 1 Tbsp Olive Oil. Dip fingers in water and spread 1 dough ball across stone. Do not touch stone! You may have to dip fingers several times and work dough by pushing and spreading to the ends of the stone at the same time.",
+	"Bake pizza dough on stone for 15 minutes at 425 degrees.",
+	"Remove from oven and cover edges of dough with olive oil and sprinkle lightly with garlic salt. Spread about 3/4 cup sauce, then about 1 cup Mozzarella cheese, then about 1/2 of the ground beef mixture. Top with more Mozzarella cheese and sprinkle with grated Italian cheeses.",
+	"Broil pizza on stone for 5-7 minutes or until the cheeses get browned and maybe even bubbly.",
+	"Remove pizza from oven and cool on wire rack.",
+	"Slide pizza onto cutting board and slice into 8 pieces",
+	"Enjoy!",
+ ],
+    imageUrl: process.env.PUBLIC_URL + '/images/BurgerPizza.png',
+    videoUrl: "https://youtube.com/embed/TcYXqwJLRFM", // Correct syntax
+  },
+  {
+    id: 7,
+    name: "Beer Cheese Dip",
+	description:"Beer cheese dip is perfect for soft pretzels or chips!",
+	ingredients: [
+	"3 Tbsp grass fed butter",
+	"3 Tbsp organic unbleached all purpose flour",
+	"1 cup beer ",
+	"2 to 3 cups grated extra sharp cheddar cheese ",
+	"1 Tbsp Worsterchire sauce",
+	"3 shakes Tobasco sauce",
+	"1 tsp dried garlic powder",
+	"1 tsp dried onion powder",
+	"1/4 tsp ground black pepper",
+	"1 tsp kosher salt",
+	"1 tsp dried parsley",
+],
+steps: [
+	"Melt butter in skillet on medium-high heat ",
+	"Whisk in flour until well combined",
+	"Whisk in beer and continue to whisk over medium heat until the solution thickens and starts to boil. Remove from heat.",
+	"Add in shredded cheese and mix until melted.",
+	"Add in Worsterchire sauce and Tabasco sauce, herbs and spices.",
+	"Return to low heat and stir all until velvety and smooth. The dip should be creamy and flow dripping from spoon.",
+	"Enjoy warm with soft pretzels or chips!",
+ ],
+ imageUrl: process.env.PUBLIC_URL + '/images/BeerCheeseDip.png',
+    videoUrl: "https://youtube.com/embed/2aNlXPpVTBs", // Correct syntax
+	
+},		
+{
+  id:8,
+	name:"Baked Salmon Dinner",
+	description:"This is a spontaneous dinner of baked sockeye salmon with fresh green beans and baked potatoes after a shopping trip to Costco.",
+
+	ingredients: [
+	"2 lbs Wild Sockeye Salmon - previously frozen",
+	"1 fresh lemon or lime",
+	"1 tsp garlic powder",
+	"1 tsp kosher salt",
+	"1/8 tsp ground black pepper",
+	"2 Tbsp Extra Virgin Olive Oil",
+	"1/4 tsp crushed rosemary",
+	"1-2 Tbsp Honey",
+	"4 small to medium baking potatoes - washed",
+	"8 pats of butter (about 1/3 Tbsp each)",
+	"4 cups fresh green beans",
+	"1 Tbsp Extra Virgin Oil Oil",
+	"1/3 cup water",
+	"salt and pepper to taste",
+	"optional: sour cream and shredded cheddar cheese for potatoes",
+],
+steps: [
+	"Preheat oven to 400 degrees.",
+	"Season salmon in large pan with juice of 1/2 lemon, salt, garlic, black pepper, crushed rosemary, Olive Oil, and honey. Run into salmon. Wash hands well and place salmon in oven on the middle rack. Let bake until the thickest part tests at 145 degrees.",
+	"Microwave 4 potatoes on plate for 10 minutes turning halfway. When finished, place on top rack of oven above the salmon. Let bake until soft to squeeze.",
+	"Place fresh washed green beans into cast iron pan on medium high heat. Season with salt and pepper, drizzle with olive oil, and add water. When steam begins to form, stir beans to mix salt, pepper and olive oil throughout. Cover and let steam in boiling water for about 5 minutes or until water is evaporated. ",
+	"Remove potatoes from oven and cut each at top, squish potato up towards gash and push two pats of butter into each potato. Wrap each in foil. You can return these to oven after finishing broiling salmon to keep them warm if necessary until ready to serve.",
+	"Switch salmon to top rack and broil for no more than 3 minutes just to get the honey and olive oil on top a little caramelized. Set aside and let rest until ready to serve.",
+	"When water has evaporated from beans, remove cover and let green beans scorch just a little for color and flavor. Remove from heat and cover until ready to serve.",
+	"Enjoy!",
+ ],
+imageUrl: process.env.PUBLIC_URL + '/images/BakedSalmonDinner.png',
+    videoUrl: "https://youtube.com/embed/hgOAWML--pQ", // Correct syntax
   }
 ];
-
-export default sampleRecipes;
