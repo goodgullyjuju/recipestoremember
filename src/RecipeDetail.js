41e5a2bf87ec8a5966227f713e4c173b5b08dc23
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-02-19 11:13:58 -0500

    update 'RecipeDetail'

--- src/RecipeDetail.js
+++ src/RecipeDetail.js
@@ -25,7 +25,45 @@ function RecipeDetail() {
 
   return (
     <div>
-      {/* Rendering logic remains unchanged */}
+      <h2>{recipe.name}</h2>
+      <p>{recipe.description}</p>
+      {recipe.ingredients && (
+        <div>
+          <h3>Ingredients</h3>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {recipe.steps && (
+        <div>
+          <h3>Steps</h3>
+          <ol>
+            {recipe.steps.map((step, index) => (
+              <li key={index}>{step}</li>
+            ))}
+          </ol>
+        </div>
+      )}
+      {recipe.imageUrl && (
+        <img src={recipe.imageUrl} alt={recipe.name} style={{ maxWidth: '100%', height: 'auto' }} />
+      )}
+      {recipe.videoUrl && (
+        <div>
+          <h3>Tutorial Video</h3>
+          <iframe
+            width="560"
+            height="315"
+            src={recipe.videoUrl}
+            title="YouTube video player"
+            style={{ border: 0 }}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 }
